5ee52b22087aa42366d97ad45b28ab61c79f861a
Tomas Castello <tomas.caste4@gmail.com>
2023-12-28 16:11:38 +0100

    Start programming DTO

---
+++ Backend/com.AtomyCompany.AtomycApp/src/main/java/com/AtomyCompany/AtomycApp/DTO/AssistantDTO.java
@@ -0,0 +1,64 @@
+package com.AtomyCompany.AtomycApp.DTO;
+
+import com.AtomyCompany.AtomycApp.model.Assistant;
+import com.AtomyCompany.AtomycApp.model.Event;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.ToString;
+import org.jetbrains.annotations.Nullable;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class AssistantDTO implements Serializable {
+
+    private static final long serialVersionUID = 4L;
+
+    // DTO parameters
+    private Long idAssistant;
+    private String assistantName;
+    private int assistantPhone;
+    private String assistantEmail;
+    private String assistantAddress;
+    private int assistantAge;
+    private Date registAssistant;
+
+    // Events attended.
+    @ToString.Exclude
+    @JsonIgnore
+    private List<Event> eventAssist = new ArrayList<>();
+
+    // Function that converts the object entity to dto
+    public static AssistantDTO convertToDTO(Assistant assistant){
+
+        AssistantDTO assistantDTO = new AssistantDTO();
+
+        assistantDTO.setIdAssistant(assistant.getIdAssistant());
+        assistantDTO.setAssistantName(assistant.getAssistantName());
+        assistantDTO.setAssistantPhone(assistant.getAssistantPhone());
+        assistantDTO.setAssistantEmail(assistant.getAssistantEmail());
+        assistantDTO.setAssistantAddress(assistant.getAssistantAddress());
+        assistantDTO.setAssistantAge(assistant.getAssistantAge());
+        assistantDTO.setRegistAssistant(assistant.getRegistAssistant());
+
+        return assistantDTO;
+    }
+
+    // Function that converts the dto object to an entity
+    public static @Nullable Assistant convertToEntity(AssistantDTO assistantDTO){
+
+        Assistant assistant = new Assistant();
+
+        assistant.setIdAssistant(assistantDTO.getIdAssistant());
+        assistant.setAssistantName(assistantDTO.getAssistantName());
+        assistant.setAssistantPhone(assistantDTO.getAssistantPhone());
+        assistant.setAssistantEmail(assistantDTO.getAssistantEmail());
+        assistant.setAssistantAddress(assistantDTO.getAssistantAddress());
+        assistant.setAssistantAge(assistantDTO.getAssistantAge());
+        assistant.setRegistAssistant(assistantDTO.getRegistAssistant());
+
+        return null;
+    }
+}
---
+++ Backend/com.AtomyCompany.AtomycApp/src/main/java/com/AtomyCompany/AtomycApp/DTO/EventDTO.java
@@ -0,0 +1,76 @@
+package com.AtomyCompany.AtomycApp.DTO;
+
+import com.AtomyCompany.AtomycApp.model.Assistant;
+import com.AtomyCompany.AtomycApp.model.Contracting;
+import com.AtomyCompany.AtomycApp.model.Event;
+import com.AtomyCompany.AtomycApp.model.GroupEvents;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class EventDTO implements Serializable {
+
+    private static final long serialVersionUID = 4L;
+
+    private Long idEvent;
+    private String eventName;
+    private Date eventDate;
+    private int eventPrice;
+    private int assistantPrice;
+    private Date registEvent;
+
+    @ToString.Exclude
+    @JsonIgnore
+    private List<Assistant> assistantsEvent = new ArrayList<>();
+
+    @ToString.Exclude
+    @JsonIgnore
+    private List<Contracting> contractions = new ArrayList<>();
+
+    @ToString.Exclude
+    @JsonIgnore
+    private GroupEvents groupEvents;
+
+    public static EventDTO convertToDTO(Event event, GroupEvents groupEvents){
+
+        EventDTO eventDTO = new EventDTO();
+
+        eventDTO.setIdEvent(event.getIdEvent());
+        eventDTO.setEventName(event.getEventName());
+        eventDTO.setEventDate(event.getEventDate());
+        eventDTO.setEventPrice(event.getEventPrice());
+        eventDTO.setAssistantPrice(event.getAssistantPrice());
+        eventDTO.setRegistEvent(event.getRegistEvent());
+
+        if (groupEvents != null){
+            eventDTO.setGroupEvents(groupEvents);
+        }else{
+            if (event.getGroupEvents() != null){
+                eventDTO.setGroupEvents(event.getGroupEvents());
+            }
+        }
+
+        return eventDTO;
+    }
+
+    public static Event convertToEntity(EventDTO eventDTO){
+
+        Event event = new Event();
+
+        event.setIdEvent(eventDTO.getIdEvent());
+        event.setEventName(eventDTO.getEventName());
+        event.setEventDate(eventDTO.getEventDate());
+        event.setEventPrice(eventDTO.getEventPrice());
+        event.setAssistantPrice(eventDTO.getAssistantPrice());
+        event.setRegistEvent(eventDTO.getRegistEvent());
+
+        return event;
+    }
+
+}
